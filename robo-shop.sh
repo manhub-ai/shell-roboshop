3cde90efbfb1dd5a8a748a836e4bcd14595e6822
Manjunatha <manjunathacloud1657@gmail.com>
2025-09-27 22:05:05 +0530

    new repo

--- robo-shop.sh
+++ robo-shop.sh
@@ -2,6 +2,8 @@
 
 AMI_ID="ami-09c813fb71547fc4f"
 SECURITY_ID="sg-062ee2abee617cab7" # replace with your Security ID and allow all traffic
+ZONE_ID="Z011643715JP8CMWCW5FX" # replace with your hosted zone ID
+DOMAIN_NAME="manjunatha.space"
 	
 for instance in $@
 do
@@ -9,8 +11,31 @@ do
 
     if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text )
+        RECORD_NAME="$instance.$DOMAIN_NAME"
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text )
+        RECORD_NAME="$DOMAIN_NAME"
     fi
     echo "$instance: $IP"
+
+    # Creates route 53 records based on env name
+
+        aws route53 change-resource-record-sets \
+        --hosted-zone-id $ZONE_ID \
+        --change-batch '
+        {
+            "Comment": "Testing creating a record set"
+            ,"Changes": [{
+            "Action"              : "UPSERT"
+            ,"ResourceRecordSet"  : {
+                "Name"              : "'$RECORD_NAME'"
+                ,"Type"             : "A"
+                ,"TTL"              : 1
+                ,"ResourceRecords"  : [{
+                    "Value"         : "'" $IP "'"
+                }]
+            }
+            }]
+        }
+        '
 done
