380d9f8601e6563bd84ab65ed72d2dd48652e8f9
Manjunatha <manjunathacloud1657@gmail.com>
2025-09-27 21:15:05 +0530

    new repo

--- robo-shop.sh
+++ robo-shop.sh
@@ -5,7 +5,12 @@ SECURITY_ID="sg-062ee2abee617cab7" # replace with your Security ID and allow all
 	
 for instance in $@
 do
-    aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-062ee2abee617cab7  --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Test}]' --query 'Instances[0].PublicIpAddress' --output text	
+    INSTANCE_ID=(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids sg-062ee2abee617cab7  --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Test}]' --query 'Instances[0].PublicIpAddress' --output text)
 
+    if [ $instance -ne "frontend" ]; then
+        PRIVATE_IP=(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text )
+    else
+        PUBLIC_IP=(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text )
+    fi
 
 done
