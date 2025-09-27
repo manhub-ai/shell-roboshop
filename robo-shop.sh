39927a15e7114d42977b480679f6fcdf8026ad01
Manjunatha <manjunathacloud1657@gmail.com>
2025-09-27 21:23:23 +0530

    new repo

--- robo-shop.sh
+++ robo-shop.sh
@@ -7,7 +7,7 @@ for instance in $@
 do
     INSTANCE_ID=(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids sg-062ee2abee617cab7  --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Test}]' --query 'Instances[0].PublicIpAddress' --output text)
 
-    if [ $instance -ne "frontend" ]; then
+    if [ $instance != "frontend" ]; then
         IP=(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text )
     else
         IP=(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text )
