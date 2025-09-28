c7daeda86545838b639a47f40d672ddd6b5e1fa4
Manjunatha <manjunathacloud1657@gmail.com>
2025-09-28 12:50:57 +0530

    save changes

---
+++ roboshop.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-062ee2abee617cab7" # replace with your SG ID
+ZONE_ID="Z011643715JP8CMWCW5FX" # replace with your ID
+DOMAIN_NAME="manjunatha.space"
+
+
+ffor instance in $@ # mongodb redis mysql
+do
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+
+    # Get Private IP
+    if [ $instance != "frontend" ]; then
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.manjunatha.space
+    else
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        RECORD_NAME="$DOMAIN_NAME" # manjunatha.space
+    fi
+
+    echo "$instance: $IP"
+
+    
+done
