328c74445afae27e5c58d156d4d41718f86aad59
Manjunatha <manjunathacloud1657@gmail.com>
2025-09-28 11:49:03 +0530

    save changes

--- robo-shop.sh
+++ robo-shop.sh
@@ -4,23 +4,24 @@ AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-062ee2abee617cab7" # replace with your SG ID
 ZONE_ID="Z011643715JP8CMWCW5FX" # replace with your ID
 DOMAIN_NAME="manjunatha.space"
+AWS="/usr/local/bin/aws"
 
 for instance in $@ # mongodb redis mysql
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+    INSTANCE_ID=$($AWS ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
     # Get Private IP
     if [ $instance != "frontend" ]; then
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        IP=$($AWS ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
         RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.manjunatha.space
     else
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        IP=$($AWS ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
         RECORD_NAME="$DOMAIN_NAME" # manjunatha.space
     fi
 
     echo "$instance: $IP"
 
-    aws route53 change-resource-record-sets \
+    $AWS route53 change-resource-record-sets \
     --hosted-zone-id $ZONE_ID \
     --change-batch '
     {
